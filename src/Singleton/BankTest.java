98be1600a2e104bf12c1a4bfc1ddc80af4f626c5
Minmengtao <2909338187@qq.com>
2021-05-12 22:40:37 +0800

    slight update

--- src/Singleton/BankTest.java
+++ src/Singleton/BankTest.java
@@ -1,10 +1,47 @@
 package Singleton;
 
+/**
+ * 从头到尾只生成一个实例，故称为单例模式
+ * 只要对象只能对一个实例进行操作，例如数据库当中的连接操作，只能对一个数据库操作，这个时候必须使用单例模式
+ * @author minmengtao
+ * @date 2020-5-12
+ */
 public class BankTest{
     public static void main(String[] args) {
         Bank b1 = Bank.getInstance();
         Bank b2 = Bank.getInstance();
-        System.out.println(b1 == b2);
+        System.out.println(b1 == b2);//return true;
+
+        Bank1 b11 = Bank1.getInstance();
+        Bank1 b12 = Bank1.getInstance();
+        System.out.println(b11 == b12);//return true;
+    }
+}
+//懒汉式，线程不安全，
+class Bank1 {
+    //延迟私有静态变量实例化
+    //1.私有化构造器
+    private Bank1() {
+        //
+    }
+
+    //2.先不实例化
+    private static Bank1 instance;
+
+    //3.需要的时候才会实例化
+    public static Bank1 getInstance() {
+        if(instance == null) {
+            instance = new Bank1();
+        }
+        return instance;
+    }
+
+    //4.为例保证方法线程安全，引入synchronized关键字，但是不推荐使用该方法
+    public static synchronized Bank1 getInstance1() {
+        if(instance == null) {
+            instance = new Bank1();
+        }
+        return instance;
     }
 }
 //饿汉式构建单例模式，线程安全
